60991182e3a054285baed43da1f6edc0b11d9cce
SgtMalarkey <trogdor1155@gmail.com>
2023-04-15 15:41:47 -0400

    formating heatmap and integrating into website framework

--- website/app.py
+++ website/app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/word-vector")
+def word_vector():
+    return render_template('heatmap.html')
+
+@app.route("/faq")
+def faq():
+    return render_template('FAQ.html')
