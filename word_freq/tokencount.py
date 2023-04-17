78f1b2b269bffbda0cd9cd80fcc8e64ec4f53c5f
ksunyan <kenyansn18@gmail.com>
2023-04-17 00:17:55 -0400

    added keyword occurrence tracking

--- word_freq/tokencount.py
+++ word_freq/tokencount.py
@@ -6,6 +6,7 @@ from datetime import date
 import sqlite3
 import config
 
+# Helper functions
 
 def count_token(token, date, token_dict, corpus):
     """Checks if a token is valid (is in a corpus or begins with a capital
@@ -27,21 +28,31 @@ def count_token(token, date, token_dict, corpus):
     else:
         return False
 
-def date_from_path(filepath):
-    for i in range(2):
-        filepath = path.split(filepath)[0]
-    date_list = []
-    for i in range(3):
-        head_tail = path.split(filepath)
-        date_list.append(int(head_tail[1]))
-        filepath = head_tail[0]
-    return date(date_list[2], date_list[1], date_list[0])
+def count_keyword(token, lccn, date, ed, seq, occurrence_list, keywords):
+    if(token in keywords):
+        occurrence_list.append({
+            "string":token,
+            "date":date,
+            "lccn":lccn,
+            "ed":ed,
+            "seq":seq
+        })
 
-def lccn_from_path(filepath):
-    for i in range(5):
-        filepath = path.split(filepath)[0]
-    head_tail = path.split(filepath)
-    return head_tail[1]
+def date_from_path(filepath):
+    m = metadata_from_path(filepath)
+    return date(int(m[1]), int(m[2]), int(m[3]))
+
+def metadata_from_path(filepath):
+    """Given a filepath to a directory containing an ocr.txt file, 
+    returns a list of the last 6 directories in the filepath.
+    These correspond to the LCCN, year, month, day, edition, and sequence
+    values for that ocr.txt file."""
+    metadata = ['']*6
+    for i in range(6):
+        head_tail = path.split(filepath)
+        filepath = head_tail[0]
+        metadata[5-i] = head_tail[1]
+    return metadata
 
 if __name__ == "__main__":
 
@@ -61,9 +72,8 @@ if __name__ == "__main__":
     db_cur.execute("CREATE TABLE IF NOT EXISTS token("
     "string, month, count, "
     "PRIMARY KEY (string, month))")
-    # db_cur.execute("CREATE TABLE IF NOT EXISTS occurrence("
-    # "string, date, newspaper_id, ed, seq, snippet, "
-    # "PRIMARY KEY (string, date))")
+    db_cur.execute("CREATE TABLE IF NOT EXISTS occurrence("
+    "string, date, lccn, ed, seq)")
     db_cur.execute("CREATE TABLE IF NOT EXISTS source("
     "filepath, month, num_tokens)")
 
@@ -74,15 +84,18 @@ if __name__ == "__main__":
 
     # Load in English corpus
     corpus = set(brown.words())
+    corpus = corpus.union(keywords)
 
     # Traverse the subdirectories of root and tokenize
     token_counts = {}
     tokenized_files = []
+    keyword_occurrences = []
     tokenizer = RegexpTokenizer(r'[a-zA-Z\-]+')
     for root, dirs, files in walk(args.root):
         if 'ocr.txt' in files:
             filedate = date_from_path(root)
             filename = path.join(root, 'ocr.txt')
+            filemetadata = metadata_from_path(root)
             print(filename)
             with open(filename) as fp:
                 # Algorithm to resolve words that span two lines
@@ -100,22 +113,54 @@ if __name__ == "__main__":
                         # Case 1: The current token is a newline
                         if(tokens[ptr] == '\n'):
                             ptr += 1
-                        # Case 2: The next token is a newline (check spanning token)
+                        # Case 2: The next token is a newline (check spanning token validity)
                         elif(tokens[ptr+1] == '\n'):
                             spanning_token = tokens[ptr] + tokens[ptr+2]
                             if(count_token(spanning_token, filedate, token_counts, corpus)):
+                                if(args.keywords):
+                                    count_keyword(spanning_token, 
+                                        filemetadata[0], 
+                                        filedate,
+                                        filemetadata[4],
+                                        filemetadata[5], 
+                                        keyword_occurrences, 
+                                        keywords)
                                 ptr += 3
                             else:
                                 count_token(tokens[ptr], filedate, token_counts, corpus)
+                                if(args.keywords):
+                                    count_keyword(tokens[ptr], 
+                                        filemetadata[0], 
+                                        filedate,
+                                        filemetadata[4],
+                                        filemetadata[5], 
+                                        keyword_occurrences, 
+                                        keywords)
                                 ptr += 2
                         # Case 3: Neither the current nor the next token is a newline
                         else:
                             count_token(tokens[ptr], filedate, token_counts, corpus)
+                            if(args.keywords):
+                                    count_keyword(tokens[ptr], 
+                                        filemetadata[0], 
+                                        filedate,
+                                        filemetadata[4],
+                                        filemetadata[5], 
+                                        keyword_occurrences, 
+                                        keywords)
                             ptr += 1
                     # Handle the final token (unless it was already handled as part of
                     # a spanning token). 
                     if(tokens[ptr] != '\n'):
                         count_token(tokens[ptr], filedate, token_counts, corpus)
+                        if(args.keywords):
+                                    count_keyword(tokens[ptr], 
+                                        filemetadata[0], 
+                                        filedate,
+                                        filemetadata[4],
+                                        filemetadata[5], 
+                                        keyword_occurrences, 
+                                        keywords)
             # Count total number of valid tokens in file 
             count_sum = sum(token_counts.values()) 
             # Add to our running list of processed files
@@ -125,9 +170,10 @@ if __name__ == "__main__":
                 "num_tokens":count_sum})
 
     # Write to database
-    print("Writing to database")
+    print("Writing data to database")
+
     val =[{
-            "string":k[0][:50], 
+            "string":k[0], 
             "month":k[1].replace(day=1), 
             "count":v} 
         for k,v in token_counts.items()]
@@ -145,6 +191,15 @@ if __name__ == "__main__":
     sql = ("INSERT INTO source "
             "(filepath, month, num_tokens) "
             "VALUES(:filepath, :month, :num_tokens)")
+    
+    for i in range(0, len(val), 100):
+        print("Executing from: " + str(i))
+        db_cur.executemany(sql,val[i:i+100])
+
+    val = keyword_occurrences
+    sql = ("INSERT INTO occurrence "
+            "(string, date, lccn, ed, seq) "
+            "VALUES(:string, :date, :lccn, :ed, :seq)")
     
     for i in range(0, len(val), 100):
         print("Executing from: " + str(i))
