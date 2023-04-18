01f16d8a76b970214de43ddd2c25ab10051208cd
ksunyan <kenyansn18@gmail.com>
2023-04-17 21:02:36 -0400

    added query functionality, skeleton data viz for word frequencies

--- website/app.py
+++ website/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import mysql.connector
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -14,3 +14,29 @@ def word_vector():
 @app.route("/faq")
 def faq():
     return render_template('FAQ.html')
+
+@app.route("/word-frequency")
+def word_freq():
+    return render_template("word_freq.html")
+
+@app.post("/query")
+def word_freq_query():
+    data = request.form
+
+    raw_words = data['words']
+    date1 = data['date1']
+    date2 = data['date2']
+
+    word_list = [w.strip() for w in raw_words.split(',')]
+
+    db_con = sqlite3.connect('ChronAmWords.db')
+    db_cur = db_con.cursor()
+
+    output_array = []
+    for word in word_list:
+        sql = ("SELECT * FROM token WHERE string='" + word + "'" )
+        res = db_cur.execute(sql)
+        result_array = [{"month":row[1], "value":row[2]} for row in res.fetchall()]
+        output_array.append({"word":word,"series":result_array})
+
+    return {"dataset":output_array}
